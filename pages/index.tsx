8b6fd87e0999c747d510186d755fe35f0891eb7d
Rahul Kumar <rahul.k@smarter.codes>
2023-09-19 11:11:41 +0530

    updated chat api

--- pages/index.tsx
+++ pages/index.tsx
@@ -143,6 +143,7 @@ export default function Home() {
         },
         body: JSON.stringify({
           question,
+          enablegptfallback: toggleStatus ? 1 : 0,
           history,
         }),
       });
