d5ce787d2b2d8fde5e6e29e64905d5ca2afbd7c5
immkg <mayank.g@smarter.codes>
2023-10-14 15:28:37 +0530

    added res

--- pages/index.tsx
+++ pages/index.tsx
@@ -80,7 +80,7 @@ export default function Home() {
     // Check if the 'chat-id' query parameter is present
     if (!urlParams.has('chat-id')) {
       // Query parameter is not present, redirect to a new URL
-      getNewInstance().then(({ headers }) => window.location.href = headers.location)
+      getNewInstance().then((res) => window.location.href = res?.headers?.location)
     }
 
     const createNewChatRoom = () => {
