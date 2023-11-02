6d86d555c16448a227660c8e134434c161d4cbec
tanmay.k <tanmay.k@smarter.codes>
2023-11-01 20:27:33 -0400

    radio button complete

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,30 @@
-import Button from "@/components/ui/Buttons/Button";
-import "../custom/CSSFile/default/Index.module.css";
+import Button from '@/components/ui/Buttons/Button';
+import '../custom/CSSFile/default/Index.module.css';
+import RadioGroup from '@/components/ui/Radio/RadioGroup';
+import { useState } from 'react';
 
 export default function Home() {
-  return(
+  const options = [
+    { label: 'YES', value: 'YES' },
+    { label: 'NO', value: 'NO' },
+  ];
+
+  const [selectedValue, setSelectedValue] = useState('YES');
+
+  const handleRadioChange = (value: string) => {
+    setSelectedValue(value);
+  };
+
+  return (
     <>
-    <Button variant="ghost">
-      Hello World!
-    </Button>
+      <Button>+ Professional Registeration</Button>
+      <Button variant="secondary">+ Professional Registeration</Button>
+      <Button variant="ghost">+ Professional Registeration</Button>
+      <RadioGroup
+        options={options}
+        selectedValue={selectedValue}
+        onChange={handleRadioChange}
+      />
     </>
-  )
+  );
 }
