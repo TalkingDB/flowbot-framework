cf45fc5e9bd70eccaa4232a48d8227fda6371a05
Rahul Kumar <rahul.k@smarter.codes>
2023-12-11 20:32:35 +0530

    remove provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,12 @@ import { SessionProvider } from 'next-auth/react';
 import '@/styles/base.css';
 import type { AppProps } from 'next/app';
 import "../configuration/CSS/Fonts.css"
-import Providers from '@/components/Providers';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Providers>
       <main style={{ fontFamily: 'Aspekta, sans-serif' }}>
         <Component {...pageProps} />
       </main>
-    </Providers>
   );
 }
 
