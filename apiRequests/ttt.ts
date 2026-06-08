550e3578044cfbe559e7f5fb4ed09d69bdf84f88
safeer-m <safeer.m@smarter.codes>
2026-06-08 11:01:11 +0530

    feat: updated the chatbot backend capability to support document uploading to TTT

---
+++ apiRequests/ttt.ts
@@ -0,0 +1,17 @@
+import { axiosTTTInstance } from "@/utils/axiosInstance"
+
+export const uploadDocument = async (file: File) => {
+    try {
+        const formData = new FormData();
+        formData.append('file', file);
+        const response = await axiosTTTInstance.post(`/v1/documents`, formData);
+        return response?.data;
+    } catch (error) {
+        console.log(`something went wrong during uploading pdf`, {
+            message: error?.message,
+            status: error?.response?.status,
+            responseData: error?.response?.data
+        });
+        return false;
+    }
+}
--- utils/axiosInstance.ts
+++ utils/axiosInstance.ts
@@ -4,6 +4,16 @@ import config from '@/config/constants';
 const backendConnectorHost = config.NEXT_PUBLIC_BACKEND_CONNECTOR_HOST
 const backendConnectorKey = config.NEXT_PUBLIC_BACKEND_CONNECTOR_KEY
 const whatsAppbackendConnectorHost = config.NEXT_PUBLIC_BACKEND_TEXT_SIMILARITY
+const TDB_TTT_SERVICE = config.TDB_TTT_SERVICE
+const TDB_TTT_SERVICE_AUTHORIZATION = config.TDB_TTT_SERVICE_AUTHORIZATION
+
+export const axiosTTTInstance = axios.create({
+    baseURL: TDB_TTT_SERVICE,
+    headers: {
+        'Authorization': `Bearer ${TDB_TTT_SERVICE_AUTHORIZATION}`,
+        'Accept': 'application/json',
+    },
+});
 
 export const axiosPDFInstance = axios.create({
     baseURL: `${backendConnectorHost}`,
